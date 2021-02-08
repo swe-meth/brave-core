2445da066dc37f167b978d6cfcb8d0d582f636e5
Mario Sanchez Prada <mario@igalia.com>
2021-02-08 17:00:58 +0100

    Update //chrome/common/url_constants.cc override.
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/616ef2d03906107acc6f7fec1fe8a9edb97b7c09
    
    commit 616ef2d03906107acc6f7fec1fe8a9edb97b7c09
    Author: Reilly Grant <reillyg@chromium.org>
    Date:   Tue Feb 2 03:17:48 2021 +0000
    
        Add help center links to the serial and HID choosers
    
        This change adds a help button to the WebHID and Web Serial API chooser
        dialogs which link to Chrome help center pages for these APIs.
    
        Bug: 1085149
        Change-Id: Icbc619db8aeb18e87d6692e327d4ed9279076ab1

--- chromium_src/chrome/common/url_constants.cc
+++ chromium_src/chrome/common/url_constants.cc
@@ -32,6 +32,12 @@ const char kCastNoDestinationFoundURL[] =
 const char kChooserBluetoothOverviewURL[] =
     "https://support.brave.com/";
 
+const char kChooserHidOverviewUrl[] =
+    "https://support.brave.com/";
+
+const char kChooserSerialOverviewUrl[] =
+    "https://support.brave.com/";
+
 const char kChooserUsbOverviewURL[] =
     "https://support.brave.com/";
 
