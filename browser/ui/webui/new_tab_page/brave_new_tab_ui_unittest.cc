263d597631e99e68b8903e6adaf76edcab4c6ccf
Simon Hong <shong@brave.com>
2021-02-20 13:59:45 +0900

    Re-enable unit test on all platforms

--- browser/ui/webui/new_tab_page/brave_new_tab_ui_unittest.cc
+++ browser/ui/webui/new_tab_page/brave_new_tab_ui_unittest.cc
@@ -11,11 +11,9 @@ TEST(BraveNewTabUITest, ConstantsTest) {
   // Make sure history/ntp_tiles module has proper constants for our NTP
   // requirements.
   constexpr size_t kBraveMaxTopSitesNumber = 12;
+  constexpr size_t kTopSitesNumber = history::TopSitesImpl::kTopSitesNumber;
 
-  // TODO(simonhong): Why this has undefined symbol link error on mac & linux?
-#if defined(OS_WIN)
-  EXPECT_EQ(kBraveMaxTopSitesNumber, history::TopSitesImpl::kTopSitesNumber);
-#endif
+  EXPECT_EQ(kBraveMaxTopSitesNumber, kTopSitesNumber);
   EXPECT_EQ(kBraveMaxTopSitesNumber, ntp_tiles::kMaxNumCustomLinks);
   EXPECT_EQ(kBraveMaxTopSitesNumber, ntp_tiles::kMaxNumMostVisited);
   EXPECT_EQ(static_cast<int>(kBraveMaxTopSitesNumber), ntp_tiles::kMaxNumTiles);
